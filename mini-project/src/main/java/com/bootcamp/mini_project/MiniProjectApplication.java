f22d679b4beb2e7afc47db8fd444674317bc3b29
a6iyyu <mizukinako7@gmail.com>
2026-09-13 13:39:14 +0700

    chore: Remove auto-generated vaadin frontend files from tracking.

--- mini-project/src/main/java/com/bootcamp/mini_project/MiniProjectApplication.java
+++ mini-project/src/main/java/com/bootcamp/mini_project/MiniProjectApplication.java
@@ -3,7 +3,9 @@ package com.bootcamp.mini_project;
 import com.vaadin.flow.component.page.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @Push
 @SpringBootApplication
 public class MiniProjectApplication implements AppShellConfigurator {
